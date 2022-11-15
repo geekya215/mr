720d7fcbcb86cbf382a2ebb1235e6a5ac93deee6
Lin Yang <geekya215@gmail.com>
2022-11-15 17:03:08 +0800

    implement sequential word count mapreduce

--- mr.go
+++ mr.go
@@ -1,7 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type KeyValue struct {
+	Key   string
+	Value string
+}
+
+type ByKey []KeyValue
+
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+func ihash(key string) int {
+	h := fnv.New32a()
+	h.Write([]byte(key))
+	return int(h.Sum32() & 0x7fffffff)
+}
+
+func Map(filename string, contents string) []KeyValue {
+	ff := func(r rune) bool { return !unicode.IsLetter(r) }
+
+	words := strings.FieldsFunc(contents, ff)
+
+	kva := []KeyValue{}
+	for _, w := range words {
+		kv := KeyValue{w, "1"}
+		kva = append(kva, kv)
+	}
+	return kva
+}
+
+func Reduce(key string, values []string) string {
+	return strconv.Itoa(len(values))
+}
 
 func main() {
-	fmt.Printf("Hello, mr!")
+	fmt.Printf("Hello, mr!\n")
+
+	files := os.Args[1:]
+	var intermediate []KeyValue
+
+	for _, filename := range files {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", filename)
+		}
+		content, err := ioutil.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read %v", filename)
+		}
+		file.Close()
+		kva := Map(filename, string(content))
+		intermediate = append(intermediate, kva...)
+	}
+
+	sort.Sort(ByKey(intermediate))
+
+	oname := "mr-out-0"
+	ofile, _ := os.Create(oname)
+
+	i := 0
+	for i < len(intermediate) {
+		j := i + 1
+		for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+			j++
+		}
+		var values []string
+		for k := i; k < j; k++ {
+			values = append(values, intermediate[k].Value)
+		}
+		output := Reduce(intermediate[i].Key, values)
+
+		fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+		i = j
+	}
+
+	ofile.Close()
 }
